8c7454cee98ee89b5a2f94296aaf233a593c2014
windowsuser404 <tinkulogesh@gmail.com>
2023-08-06 06:30:56 +0530

    Tested parallel working

---
+++ testing_parallel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+folder="/home/gol/Parallel_processing/Data_files"
+for file in "$folder"/*.txt; do
+	echo "File:$file"
+	max=$(awk 'BEGIN{max=0} {if ($1>max) max=$1; if ($2>max) max=$2} END{print max}' "$file")
+        min=$(awk 'BEGIN{min=999999998} {if ($1<min) min=$1; if ($2<min) min=$2} END{print min}' "$file")
+	./louvian_parallel.out ${file} ${max}
+	max=0
+	min=999999997
+	echo "-------------------------------------------------------------------------------------------"
+done | tee result_parallel.txt
